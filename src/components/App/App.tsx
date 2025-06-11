d0f38c73a293e272147d200cd1c54e9525e1ce1e
Сергій Власов <vikinglaurasun@gmail.com>
2025-06-11 16:29:03 +0300

    start homework 0-5

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+
+import css from './App.module.css'
+
+<div className={css.app}>
+	<header className={css.toolbar}>
+		{/* Компонент SearchBox */}
+		{/* Пагінація */}
+		{/* Кнопка створення нотатки */}
+  </header>
+</div>
+
+
+export default App
